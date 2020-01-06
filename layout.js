fcf6f8ef7c2d0de6a5a9552515a928a911b7bfbd
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-01-06 15:27:40 -0700

    initial layout

---
+++ layout.js
@@ -0,0 +1,35 @@
+// build pop up module
+
+// popup
+var popup = document.getElementById('initPopUp')
+
+// create fields needed
+// player name
+var playerNameText = document.createElement('a')
+playerNameText.innerHTML = 'Player Name: '
+var playerName = document.createElement('input')
+playerName.type = 'text-box'
+playerName.placeholder = 'Fist Last'
+// Current Level
+var currentLevelText = document.createElement('a')
+currentLevelText.innerHTML = "Current Level: "
+var currentLevel = document.createElement('input')
+currentLevel.type = 'number'
+currentLevel.placeholder = 0
+// Current XP
+var currentXpText = document.createElement('a')
+currentXpText.innerHTML = 'Current XP: '
+var currentXp = document.createElement('input')
+currentXp.type = 'number'
+currentXp.placeholder = 0
+
+// race
+// class
+// current money
+// current inventory
+    // armor
+    // weapons
+    // other
+// current hit points
+
+// need to have character select option!
---
+++ popUpSubmit.js
@@ -0,0 +1,13 @@
+function popUpSubmit(){
+    // start loader icon
+    // get all player information
+    // validate all player information
+        // if it is invalid give warning message and exit function
+    // save player information
+        // if error display error and have user re-try
+    // if player is a spell caster load spell pop up
+    // populate main page
+    // hide popup
+    // loader off 
+
+}
