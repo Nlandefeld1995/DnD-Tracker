3f09fd5739fd3bc8ea3d07588177dfd914291393
cbehling49 <59753794+cbehling49@users.noreply.github.com>
2020-01-10 21:41:54 -0700

    created dropdowns for class and race

--- loadLayout.js
+++ loadLayout.js
@@ -91,23 +91,42 @@ function newCharacter() {
     // race
     var raceText = document.createElement('a')
     raceText.innerHTML = 'Race: '
-    var race = document.createElement('input')
-    race.type = 'text-box'
-    race.placeholder = 'Lets make this a drop down'
-    race.id = 'playerRace'
+    var raceDropDown = document.createElement('select')
+    raceDropDown.id='raceType'
+    var raceArray = ['Aarakocra','Aasimar','Bugbear','Centaur','Changeling','Dragon Born','Dwarf','Elf','Feral Tiefling','Firbolg','Genasi','Gith','Gnome','Goblin','Goliath','Half-Elf','Half-Orc','Halfling','Hobgoblin','Human','Kalashtar','Kenku','Kobold','Lizardfolk','Locathah','Loxodon','Minotaur','Orc','Orc of Eberron','Shifter','Simic Hybrid','Tabaxi','Tiefling','Tortle','Triton','Warforged','Vedalken','Verdan','Yuan-ti Pureblood']
+    raceArray = raceArray.sort()
+    var raceDefault = document.createElement('option')
+        raceDefault.value = 'Select'
+        raceDefault.innerHTML = 'Select'
+    raceDropDown.appendChild(raceDefault)
+    for(i = 0; i<raceArray.length; i++){
+        var raceOptions = document.createElement('option')
+        raceOptions.value = raceArray[i]
+        raceOptions.innerHTML = raceArray[i]
+        raceDropDown.appendChild(raceOptions)
+    }
     newCharacterContainer.appendChild(raceText)
-    newCharacterContainer.appendChild(race)
+    newCharacterContainer.appendChild(raceDropDown)
 
     // class
     var classText = document.createElement('a')
     classText.innerHTML = 'Class: '
-    var playerClass = document.createElement('input')
-    playerClass.type = 'text-box'
-    playerClass.id = 'playerClass'
-    playerClass.placeholder = 'Lets make this a drop down'
-
+    var classDropDown = document.createElement('select')
+    classDropDown.id='classType'
+    var classArray = ['Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard','Artificer','Blood Hunter']
+    classArray = classArray.sort()
+    var classDefault = document.createElement('option')
+        classDefault.value = 'Select'
+        classDefault.innerHTML = 'Select'
+    classDropDown.appendChild(classDefault)
+    for(i = 0; i<classArray.length; i++){
+        var classOptions = document.createElement('option')
+        classOptions.value = classArray[i]
+        classOptions.innerHTML = classArray[i]
+        classDropDown.appendChild(classOptions)
+    }
     newCharacterContainer.appendChild(classText)
-    newCharacterContainer.appendChild(playerClass)
+    newCharacterContainer.appendChild(classDropDown)
 
     // current money
     // current inventory
