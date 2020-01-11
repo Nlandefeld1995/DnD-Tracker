90f9e4ee2b85c75aeae5f4248487278c04d5a6f4
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-01-10 20:57:17 -0700

    updates to the main page

--- levelXp.js
+++ levelXp.js
@@ -5,6 +5,78 @@ var levelXp = [
     },
     {
         level: 2,
-        xp: 500
+        xp: 300
+    },
+    {
+        level: 3,
+        xp: 900
+    },
+    {
+        level: 4,
+        xp: 2700
+    },
+    {
+        level: 5,
+        xp: 6500
+    },
+    {
+        level: 6,
+        xp: 14000
+    },
+    {
+        level: 7,
+        xp: 23000
+    },
+    {
+        level: 8,
+        xp: 34000
+    },
+    {
+        level: 9,
+        xp: 48000
+    },
+    {
+        level: 10,
+        xp: 64000
+    },
+    {
+        level: 11,
+        xp: 85000
+    },
+    {
+        level: 12,
+        xp: 100000
+    },
+    {
+        level: 13,
+        xp: 120000
+    },
+    {
+        level: 14,
+        xp: 140000
+    },
+    {
+        level: 15,
+        xp: 165000
+    },
+    {
+        level: 16,
+        xp: 195000
+    },
+    {
+        level: 17,
+        xp: 225000
+    },
+    {
+        level: 18,
+        xp: 265000
+    },
+    {
+        level: 19,
+        xp: 305000
+    },
+    {
+        level: 20,
+        xp: 355000
     }
 ]
