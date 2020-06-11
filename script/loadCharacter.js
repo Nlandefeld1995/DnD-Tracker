a8abd1899ab66f086f948cf51636c54dbcca1e8f
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-06-11 13:09:53 -0600

    Finished column1 outline

--- script/loadCharacter.js
+++ script/loadCharacter.js
@@ -1,10 +1,11 @@
 async function loaduser(){
     let modal = document.getElementById("popupModal")
-    modal.display = 'none'
+    modal.style.display = 'none'
     let user = await dbGetUserById(globalUserId)
     let characterToLoad = user.characters[0]
-    let character = await dbGetCharacterbyId(characterToLoad)
+    character = await dbGetCharacterbyId(characterToLoad)
     console.log(character)
+    loadMainScreen()
     // get from db list of characters. Load first character
     // from db load character
 }
