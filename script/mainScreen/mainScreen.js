cf10e1791d8f781cfbdd234219bae31ab2c28a65
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-06-15 12:36:44 -0600

    Main screen (#8)
    * Finished column1 outline
    
    * updated files for main screen
    
    * Column2 Start

--- script/mainScreen/mainScreen.js
+++ script/mainScreen/mainScreen.js
@@ -1,5 +1,11 @@
 async function loadMainScreen(){
+
+    // add loader 
+    // turn on 
     let main = document.getElementById('mainContainer')
     let mainColumn1 = await column1()
+    let mainColumn2 = await column2()
     main.appendChild(mainColumn1)
+    main.appendChild(mainColumn2)
+    // loader off
 }
