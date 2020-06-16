af847c9b93992f7a5f8b6bba6bc59257e233be09
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-06-16 13:44:27 -0600

    finished styling for column 1

--- script/createCharacter/window1.js
+++ script/createCharacter/window1.js
@@ -17,13 +17,6 @@ function window1Create() {
             required: true
         },
         {
-            ask: "Character Middle Name",
-            type: "textbox",
-            id: "characterMiddleName",
-            default: character.middleName,
-            required: false
-        },
-        {
             ask: "Character Last Name",
             type: "textbox",
             id: "characterLastName",
@@ -82,10 +75,6 @@ function window1Validate() {
     else {
         error = true
         errorText += ` Please enter a first name longer than 3 characters. `
-    }
-    // If middle name add to character
-    if (valueById('characterMiddleName')) {
-        character.middleName = valueById('characterMiddleName')
     }
     // validate last name
     let lastName = valueById('characterLastName')
--- script/mainScreen/column2.js
+++ script/mainScreen/column2.js
@@ -1,17 +1,84 @@
 async function column2(){
     let column2 = document.createElement('div')
     column2.style.width = '50%'
-    column2.style.borderRight = 'solid black'
+    column2.style.borderRight = 'solid #b78846'
+    column2.style.float = 'left'
+    column2.style.paddingRight = '5px'
+    column2.style.paddingLeft = '5px'
     column2.id = 'mainColumn2'
     let playerinformation = await createPlayerInfo()
-
+    column2.appendChild(playerinformation)
     return column2
 }
 
 async function createPlayerInfo(){
-    let div = documnet.createElement('div')
-    div.id = 'playerInformationDiv'
+    let div = document.createElement('div')
+    div.id = 'playerInfoDiv'
 
+    let divRow1 = document.createElement('div')
+    divRow1.id='playerInfoRow1'
+
+    let nameDiv = document.createElement('div')
+    nameDiv.id = 'playerInfoNameDiv'
+    let nameText = document.createElement('h3')
+    nameText.innerText = `${character.firstName} ${character.lastName}`
+    nameDiv.appendChild(nameText)
+
+    let raceDiv = document.createElement('div')
+    raceDiv.id = 'playerInfoRaceDiv'
+    let raceText = document.createElement('h3')
+    raceText.innerText = character.race
+    raceDiv.appendChild(raceText)
+
+    let classDiv = document.createElement('div')
+    classDiv.id = 'playerInfoClassDiv'
+    let classText = document.createElement('h3')
+    classText.innerText = character.class 
+    classDiv.appendChild(classText)
+
+    divRow1.appendChild(nameDiv)
+    divRow1.appendChild(classDiv)
+    divRow1.appendChild(raceDiv)
+    
+    let divRow2 = document.createElement('div')
+    divRow2.id='playerInfoRow2'
+
+    let backGroundDiv = document.createElement('div')
+    backGroundDiv.id = 'playerInfoBackgroundDiv'
+    let backGroundText = document.createElement('h3')
+    backGroundText.innerText = character.background 
+    backGroundDiv.appendChild(backGroundText)
+
+    let alignmentDiv = document.createElement('div')
+    alignmentDiv.id = 'playerInfoAlignmentDiv'
+    let alignmentText = document.createElement('h3')
+    alignmentText.innerText = character.alignment
+    alignmentDiv.appendChild(alignmentText)
+
+    let levelDiv = document.createElement('div')
+    levelDiv.id = 'playerInfoLevelDiv'
+    let levelText = document.createElement('h3')
+    levelText.innerText = character.level 
+    levelDiv.appendChild(levelText)
+
+    let xpDiv = document.createElement('div')
+    xpDiv.id = 'playerInfoXpDiv'
+    let xpText = document.createElement('h3')
+    xpText.innerText = character.xp 
+    xpDiv.appendChild(xpText)
+
+    divRow2.appendChild(backGroundDiv)
+    divRow2.appendChild(alignmentDiv)
+    divRow2.appendChild(levelDiv)
+    divRow2.appendChild(xpDiv)
+
+    div.appendChild(divRow1)
+    div.appendChild(divRow2)
+
+    return div 
+}
+
+async function createNavBar(){
     let navigation = [
         {
             label: 'Hit Points',
@@ -74,6 +141,4 @@ async function createPlayerInfo(){
             order: 10
         }
     ]
-    
-
 }
