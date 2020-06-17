2e3a747f6e611a767985d1e1e0a835f0d227799f
Nolan Landefeld <nlandefeld1995@gmail.com>
2020-06-17 09:47:34 -0600

    fixed issue

--- script/mainScreen/column2.js
+++ script/mainScreen/column2.js
@@ -25,11 +25,6 @@ async function column2() {
     return column2
 }
 
-async function createPlayerInfo() {
-
-
-    return column2
-}
 
 async function createPlayerInfo(){
     let div = document.createElement('div')
@@ -150,125 +145,6 @@ async function createPlayerInfo(){
     return div 
 }
 
-
-    let div = document.createElement('div')
-    div.id = 'playerInfoDiv'
-
-
-    let divRow1 = document.createElement('div')
-    divRow1.id = 'playerInfoRow1'
-
-    let nameDiv = document.createElement('div')
-    nameDiv.id = 'playerInfoNameDiv'
-    nameDiv.className = 'playerInfoRow'
-    let nameText = document.createElement('h5')
-    nameText.innerText = 'Name:'
-    let nameValue = document.createElement('input')
-    nameValue.value = `${character.firstName} ${character.lastName}`
-    nameValue.readOnly = true
-    nameValue.className = 'inputTextBox3'
-    nameDiv.appendChild(nameText)
-    nameDiv.appendChild(nameValue)
-
-    let raceDiv = document.createElement('div')
-    raceDiv.id = 'playerInfoRaceDiv'
-    raceDiv.className = 'playerInfoRow'
-    let raceText = document.createElement('h5')
-    raceText.innerText = 'Race:'
-    let raceValue = document.createElement('input')
-    raceValue.value = character.race
-    raceValue.readOnly = true
-    raceValue.className = 'inputTextBox3'
-    raceDiv.appendChild(raceText)
-    raceDiv.appendChild(raceValue)
-
-    let classDiv = document.createElement('div')
-    classDiv.id = 'playerInfoClassDiv'
-    classDiv.className = 'playerInfoRow'
-    let classText = document.createElement('h5')
-    classText.innerText = 'Class:'
-    let classValue = document.createElement('input')
-    classValue.value = character.class
-    classValue.readOnly = true
-    classValue.className = 'inputTextBox3'
-    classDiv.appendChild(classText)
-    classDiv.appendChild(classValue)
-
-    divRow1.appendChild(nameDiv)
-    divRow1.appendChild(await createVl())
-    divRow1.appendChild(classDiv)
-    divRow1.appendChild(await createVl())
-    divRow1.appendChild(raceDiv)
-
-    let divRow2 = document.createElement('div')
-    divRow2.id = 'playerInfoRow2'
-
-    let backGroundDiv = document.createElement('div')
-    backGroundDiv.id = 'playerInfoBackgroundDiv'
-    backGroundDiv.className = 'playerInfoRow'
-    let backGroundText = document.createElement('h5')
-    backGroundText.innerText = 'Background:'
-    let backGroundValue = document.createElement('input')
-    backGroundValue.value = (character.background) ? character.background : 'N/A'
-    // backGroundValue.readOnly = false
-    backGroundValue.className = 'inputTextBox3'
-    backGroundDiv.appendChild(backGroundText)
-    backGroundDiv.appendChild(backGroundValue)
-
-    let alignmentDiv = document.createElement('div')
-    alignmentDiv.id = 'playerInfoAlignmentDiv'
-    alignmentDiv.className = 'playerInfoRow'
-    let alignmentText = document.createElement('h5')
-    alignmentText.innerText = "Alignment:"
-    let alignmentValue = document.createElement('input')
-    alignmentValue.value = (character.alignment) ? character.alignment : 'N/A'
-    // alignmentValue.readOnly = false
-    alignmentValue.className = 'inputTextBox3'
-    alignmentDiv.appendChild(alignmentText)
-    alignmentDiv.appendChild(alignmentValue)
-
-    let levelDiv = document.createElement('div')
-    levelDiv.id = 'playerInfoLevelDiv'
-    levelDiv.className = 'playerInfoRow'
-    let levelText = document.createElement('h5')
-    levelText.innerText = 'Level:'
-    let levelValue = document.createElement('input')
-    levelValue.value = character.level
-    // levelValue.readOnly = false
-    levelValue.className = 'inputTextBox3'
-    levelDiv.appendChild(levelText)
-    levelDiv.appendChild(levelValue)
-
-    let xpDiv = document.createElement('div')
-    xpDiv.id = 'playerInfoXpDiv'
-    xpDiv.className = 'playerInfoRow'
-    let xpText = document.createElement('h5')
-    xpText.innerText = 'XP:'
-
-    let xpValue = document.createElement('input')
-    xpValue.value = character.xp
-    // xpValue.readOnly = false
-    xpValue.className = 'inputTextBox3'
-    xpDiv.appendChild(xpText)
-    xpDiv.appendChild(xpValue)
-
-    divRow2.appendChild(backGroundDiv)
-    divRow2.appendChild(await createVl())
-    divRow2.appendChild(alignmentDiv)
-    divRow2.appendChild(await createVl())
-    divRow2.appendChild(levelDiv)
-    divRow2.appendChild(await createVl())
-    divRow2.appendChild(xpDiv)
-
-    div.appendChild(divRow1)
-    div.appendChild(divRow2)
-    function createVl() {
-        let vl = document.createElement('div')
-        vl.className = 'vl'
-        return vl
-    }
-    return div
-}
 
 
 async function createNavBar() {
